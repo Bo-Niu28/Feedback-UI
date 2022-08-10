f995a023ffe502bd041a82f23f32a7bc9076ad8b
Bo-Niu28 <michaelniu1812@gmail.com>
2022-08-10 17:00:42 +1000

    FeedbackItem

---
+++ feedback_ui/src/data/FeedbackData.jsx
@@ -0,0 +1,24 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum reiciendis necessitatibus, esse officia itaque cumque maiores, fuga, a nisi recusandae expedita animi praesentium aspernatur est!",
+  },
+  {
+    id: 2,
+    rating: 7,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum reiciendis necessitatibus, esse officia itaque cumque maiores, fuga, a nisi recusandae expedita animi praesentium aspernatur est!",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum reiciendis necessitatibus, esse officia itaque cumque maiores, fuga, a nisi recusandae expedita animi praesentium aspernatur est!",
+  },
+  {
+    id: 4,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum reiciendis necessitatibus, esse officia itaque cumque maiores, fuga, a nisi recusandae expedita animi praesentium aspernatur est!",
+  },
+];
+
+export default FeedbackData;
