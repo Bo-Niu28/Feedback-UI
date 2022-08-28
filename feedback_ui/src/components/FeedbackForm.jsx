3f5f80edc81c7823b4dd9ae6cb5f0fbfa038f777
Bo-Niu28 <michaelniu1812@gmail.com>
2022-08-28 22:22:25 +1000

    Router

--- feedback_ui/src/components/FeedbackForm.jsx
+++ feedback_ui/src/components/FeedbackForm.jsx
@@ -3,8 +3,9 @@ import Button from "./shared/Button";
 import { useState } from "react";
 import RatingSelect from "./RatingSelect";
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
   const [message, setMessage] = useState("");
   const [btnDisabled, setBtnDisabled] = useState(true);
 
@@ -22,11 +23,25 @@ function FeedbackForm() {
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text: text,
+        rating: rating,
+      };
+
+      handleAdd(newFeedback);
+
+      setText("");
+    }
+  };
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        <RatingSelect />
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             type="text"
---
+++ feedback_ui/src/components/pages/AboutPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "../shared/Card";
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h2>About Page</h2>
+        <p>This is a app build using react to add feedback for product</p>
+        <p>Version: 1.0.0</p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
